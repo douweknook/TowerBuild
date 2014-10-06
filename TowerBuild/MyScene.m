bb2b45901d7b30ca28da9adb60c1f300d53cd9c8
douweknook <douwe_knook@hotmail.com>
2014-10-06 14:47:59 +0200

    added wireframes

--- TowerBuild/MyScene.m
+++ TowerBuild/MyScene.m
@@ -14,7 +14,7 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor greenColor];
         
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
